d689d6dd987a9f30faf73185dc23602ab55d0179
Juan Madalena <juanmadalena06@gmail.com>
2024-04-04 18:20:39 +0200

    fix! matchItem

--- components/MatchItem.tsx
+++ components/MatchItem.tsx
@@ -1,30 +1,29 @@
 import { StyleSheet, View as DefaultView, Animated, TouchableOpacity } from 'react-native';
 import { router } from 'expo-router';
 
-import { Text, View, useThemeColor } from '@/components/Themed';
+import { Text, useThemeColor } from '@/components/Themed';
 import Icon from './Icon';
 import MatchDetailChip from './MatchDetailChip';
-import { Match, MatchesPlayedData } from '@/interfaces';
+import { Match } from '@/interfaces';
 import { checkIfMatchIsAvailable } from '@/utils/checkIfMatchIsAvailable';
 import { useFade } from '@/hooks/useFade';
 import { useEffect } from 'react';
-import { formatDateToString } from '@/utils/formatDateToString';
 
 interface Props {
     match: Match,
     height?: number,
     index?: number,
     checkMatches?: boolean
+    cardColor?: string
 }
 
-const MatchItem = ({ match, index = 0, height = 135, checkMatches = true }: Props) => {
+const MatchItem = ({ match, index = 0, height = 135, checkMatches = true, ...props}: Props) => {
+
+    const isActive = checkMatches ? checkIfMatchIsAvailable(match) == 'join'  ? true : false : true;
+    const { opacity, fadeIn } = useFade();
 
     const primaryColor = useThemeColor({}, 'primaryColor');
-    // const isActive = checkIfMatchIsAvailable(match) == 'join'  ? true : false;
-    const isActive = checkIfMatchIsAvailable(match) == 'join'  ? true : false;
-    // console.log('isActive', isActive)
-    // const isActive = true;
-    const { opacity, fadeIn } = useFade();
+    const { cardColor = primaryColor } = props;
 
     // fade in animation
     useEffect(() => {
@@ -32,36 +31,32 @@ const MatchItem = ({ match, index = 0, height = 135, checkMatches = true }: Prop
     }, [])
 
     return (
-        <Animated.View style={{...styles.container, backgroundColor: isActive ? primaryColor : `${primaryColor}AC`, height, opacity}}>
-            <DefaultView style={{height: '100%', width:'80%', justifyContent:'space-between', opacity: isActive ? 1 : 0.5}}>
-                <DefaultView style={{justifyContent: 'center'}}>
-                    <Text style={{fontSize:14, fontWeight:'400', opacity:0.8, color:'white'}}>{match.address}</Text>
+        <TouchableOpacity 
+            style={{backgroundColor:'transparent', alignItems: 'center', justifyContent: 'center'}}
+            activeOpacity={0.8}
+            onPress={() => router.navigate({
+                pathname: '/(app)/match/[id]',
+                params: { id: match.id },
+            })}
+        >
+            <Animated.View style={{...styles.container, backgroundColor: isActive ? cardColor : `${cardColor}AC`, height, opacity}}>
+                <DefaultView style={{height: '100%', width:'80%', justifyContent:'space-between', opacity: isActive ? 1 : 0.8}}>
+                    <DefaultView style={{justifyContent: 'center'}}>
+                        <Text style={{fontSize:14, fontWeight:'400', opacity:0.8, color:'white'}}>{match.address}</Text>
+                    </DefaultView>
+                    <DefaultView style={{justifyContent: 'center', width:'100%', height:'45%', paddingBottom:4}}>
+                        <Text style={{fontSize:120, width:'80%', fontWeight:'500', color:'white'}} numberOfLines={1} adjustsFontSizeToFit>{match.location}</Text>
+                    </DefaultView>
+                    <DefaultView style={{flexDirection:'row', width:'100%', alignItems: 'center'}}>
+                        <MatchDetailChip text={match.time.slice(0,-3)} style={{ opacity: isActive ? 1 : 0.3 }} />
+                        <MatchDetailChip text={match.num_players +' / ' + match.max_players} style={{ opacity: isActive ? 1 : 0.3 }} />
+                    </DefaultView>
                 </DefaultView>
-                <DefaultView style={{justifyContent: 'center', width:'100%', height:'45%', paddingBottom:4}}>
-                    <Text style={{fontSize:120, width:'80%', fontWeight:'500', color:'white'}} numberOfLines={1} adjustsFontSizeToFit>{match.location}</Text>
+                <DefaultView style={{width:'20%',alignItems:'flex-end', flex:1, justifyContent:'flex-end', padding:4}}>
+                    <Icon name='arrow-outward' size={25} color='white'/>
                 </DefaultView>
-                <DefaultView style={{flexDirection:'row', width:'100%', alignItems: 'center'}}>
-                    <MatchDetailChip text={match.time.slice(0,-3)} />
-                    <MatchDetailChip text={match.num_players +' / ' + match.max_players} />
-                    {/* <MatchDetailChip text={match.price + ' €'} /> */}
-                </DefaultView>
-            </DefaultView>
-            <DefaultView style={{width:'20%',alignItems:'flex-end', flex:1, justifyContent:'flex-end', padding:4}}>
-                {
-                    isActive &&
-                        <TouchableOpacity 
-                            style={{backgroundColor:'transparent', alignItems: 'center', justifyContent: 'center'}}
-                            activeOpacity={0.8}
-                            onPress={() => router.navigate({
-                                pathname: '/(app)/match/[id]',
-                                params: { id: match.id },
-                            })}
-                        >
-                            <Icon name='arrow-outward' size={25} color='white'/>
-                        </TouchableOpacity>
-                }
-            </DefaultView>
-        </Animated.View>
+            </Animated.View>
+        </TouchableOpacity>
     );
 };
 
