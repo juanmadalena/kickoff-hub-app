58ed420bff0fd56cf810cdeffa83ba121afdbcda
Juan Madalena <juanmadalena06@gmail.com>
2024-04-04 18:02:51 +0200

    format useForm hook

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -4,31 +4,12 @@ export const useForm = <T extends Object>( initState: T ) => {
     
     const [state, setState] = useState<T>( initState );
 
-    const onChange = async ( value: string | string[], field: keyof T | Array<keyof T> ) => {
-        if(Array.isArray(field)){
-            console.log("pre-loop",state)
-            for await (const fieldIndex of field) {
-                console.log("loop",fieldIndex, value[field.indexOf(fieldIndex)])
-                console.log("loop",state)
-                setState({
-                    ...state,
-                    [fieldIndex]: value[field.indexOf(fieldIndex)]
-                });
-            }
-            // field.forEach((fieldIndex, index) => {
-            //     console.log(state)
-            //     setState({
-            //         ...state,
-            //         [fieldIndex]: value[index]
-            //     });
-            // });
-        }else{
-            setState({
-                ...state,
-                [field]: value
-            });
-        }
-    }
+    const onChange = ( value: string | string[], field: keyof T) => {
+        setState({
+            ...state,
+            [field]: value
+        });
+}
 
     return {
         ...state,
