4ccefc3636dace8749c9624c669b7fc081aeb65b
Juan Madalena <juanmadalena06@gmail.com>
2024-04-04 18:19:52 +0200

    filter in matches organized and history

--- app/(app)/(tabs)/matchesHistory.tsx
+++ app/(app)/(tabs)/matchesHistory.tsx
@@ -1,42 +1,69 @@
+import { useState } from 'react';
 import { SectionList } from 'react-native';
 import { useFocusEffect } from 'expo-router';
 
 import MatchItem from '@/components/MatchItem';
 import { View, Text } from '@/components/Themed';
 import { useMatchesPlayed } from '@/hooks/useMatches';
-import { formatDateToString } from '@/utils/formatDateToString';
 import LoadingComponent from '@/components/LoadingComponent';
+import TopBarNavigator from '@/components/TopBarNavigator';
+import FiltersComponent from '@/components/FiltersComponent';
+import { filterMatches } from '@/utils/filterMatches';
+import TitleSectionList from '@/components/TitleSectionList';
+import { Match, orderOptions, showOptions } from '@/interfaces';
 
 const matchesHistory = () => {
 
     const { matchesPlayedQuery } = useMatchesPlayed();
 
+    const [ filteredData, setFilteredData ] = useState<any[]>([])
+
     useFocusEffect(() => {
         matchesPlayedQuery.refetch();
     });
 
+    const changeFilter = (filter: keyof typeof showOptions, group: keyof Match , order: keyof typeof orderOptions ) => {
+        const filtered = filterMatches({matches: matchesPlayedQuery.data?.matches!, filter, group, order})
+        setFilteredData(filtered)
+    }
+
+    if(matchesPlayedQuery.isLoading) {
+        return <LoadingComponent/>
+    }
+
+    if(matchesPlayedQuery.isError) {
+        return (
+            <View>
+                <TopBarNavigator />
+                <View style={{flex:1, width:'100%', justifyContent:'center', alignItems:'center'}}>
+                    <Text>
+                        There was an error 😞
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 
     return (
         <View style={{flex:1}}>
-            {
-                matchesPlayedQuery.isLoading && <LoadingComponent/>
-            }
-            {
-                matchesPlayedQuery.isError && <Text>Error</Text>
-            }
+            <FiltersComponent color='#172A3A' changeFilter={changeFilter} marginBottom={12} />
             {
                 matchesPlayedQuery.isSuccess && 
                     <SectionList
-                        sections={matchesPlayedQuery.data?.matches}
-                        renderItem={({item, index}) => <MatchItem match={item} index={index} checkMatches={false}/>}
+                        sections={filteredData}
+                        renderItem={({item, index}) => <MatchItem match={item} index={index} checkMatches={false} cardColor='#172A3A' />}
                         keyExtractor={(_, index) => index.toString()}
                         showsVerticalScrollIndicator={false}
                         style={{paddingHorizontal: 10, flex:1}}
+                        contentContainerStyle={{paddingBottom: 50}}
                         getItemLayout={(_, index) => ({length: 135, offset: 135 * index, index})}
+                        ListEmptyComponent={
+                            <Text style={{textAlign:'center', fontSize: 16, fontWeight: '600', marginTop:'40%'}}> 
+                                No matches found 😞
+                            </Text>
+                        }
                         renderSectionHeader={({section: {title}}) => (
-                            <View>
-                                <Text style={{fontSize: 14, paddingLeft:10, paddingVertical:10, color:'grey', fontWeight: '600'}}>{formatDateToString(title, 'dd/mm/yyyy')}</Text>
-                            </View>
+                            <TitleSectionList title={title} />
                         )}
                     />
             }
--- app/(app)/(tabs)/matchesOrganized.tsx
+++ app/(app)/(tabs)/matchesOrganized.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { FlatList, RefreshControl, View as DefaultView } from 'react-native';
+import { useMemo, useState } from 'react';
+import { FlatList, RefreshControl, View as DefaultView, StyleSheet, Animated, Easing, SectionList } from 'react-native';
 
-import { View, Text, Button } from '@/components/Themed';
+import { View, Text, Button, useThemeColor } from '@/components/Themed';
 import { useMatchesOrganized } from '@/hooks/useMatches';
 import BottomModal from '@/components/BottomModal';
 import LoadingComponent from '@/components/LoadingComponent';
@@ -9,6 +9,10 @@ import MatchItem from '@/components/MatchItem';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import TopBarNavigator from '@/components/TopBarNavigator';
 import { useRouter } from 'expo-router';
+import FiltersComponent from '@/components/FiltersComponent';
+import { showOptions, orderOptions, Match } from '@/interfaces';
+import { filterMatches } from '@/utils/filterMatches';
+import TitleSectionList from '@/components/TitleSectionList';
 
 const matchesOrganized = () => {
 
@@ -16,8 +20,11 @@ const matchesOrganized = () => {
 
     const { matchesOrganizedQuery } = useMatchesOrganized();
     const { bottom } = useSafeAreaInsets();
+    const [ filteredData, setFilteredData ] = useState<any[]>([])
 
     const router = useRouter();
+
+    const buttonColor = useThemeColor({}, 'secondaryColor');
 
     if(matchesOrganizedQuery.isLoading) {
         return( 
@@ -26,29 +33,72 @@ const matchesOrganized = () => {
         </View>)
     };
 
+    const changeFilter = (filter: keyof typeof showOptions, group: keyof Match , order: keyof typeof orderOptions ) => {
+        const filtered = filterMatches({matches: matchesOrganizedQuery.data?.matches!, filter, group, order})        
+        setFilteredData(filtered)
+    }
+
+
+    // Animation rotate loop
+    const spinValue = new Animated.Value(0);
+    Animated.loop(
+        Animated.timing(
+            spinValue,
+            {
+                toValue: 1,
+                duration: 2800,
+                useNativeDriver: true,
+                isInteraction: false,
+                delay: 0,
+                easing: Easing.linear
+            }
+        )
+    ).start();
+
+    //interpolalte the value of spinValue
+    const spin = spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg']
+    });
+
     return (
         <>
-            <View>
+            <View style={{height:'100%'}}>
+                <FiltersComponent color='#004759' changeFilter={changeFilter} />
                 {
-                    matchesOrganizedQuery.isSuccess &&
-                    <FlatList
-                        data={matchesOrganizedQuery.data.matches}
+                    matchesOrganizedQuery.isSuccess && filteredData && filteredData?.length > 0 &&
+                    <SectionList
+                        sections={filteredData}
                         refreshControl={<RefreshControl refreshing={false} onRefresh={matchesOrganizedQuery.refetch}/>}
-                        renderItem={({ item }) => <MatchItem match={item} /> }
+                        renderItem={({ item }) => <MatchItem match={item} checkMatches={false} cardColor='#004759' /> }
                         keyExtractor={item => item.id}
                         showsVerticalScrollIndicator={false}
-                        style={{ paddingHorizontal: 10 }}
+                        style={{paddingHorizontal: 10, flex:1, height:'100%'}}
+                        contentContainerStyle={{paddingBottom: 130}}
+                        ListEmptyComponent={
+                            <Text style={{textAlign:'center', fontSize: 16, fontWeight: '600', marginTop:'40%'}}> 
+                                No matches found 😞
+                            </Text>
+                        }
+                        renderSectionHeader={ ( {section} ) => (
+                            <TitleSectionList title={section.title} />
+                        )}
                     />
                 }
-                <DefaultView style={{position:'absolute', bottom: bottom + 10, left:'30%', right:'30%'}} >
-                    {/* <Button style={{alignItems:'center', justifyContent:'space-between', flexDirection: 'row', height:40, borderRadius:40}} onPress={() => setShowNewMatchModal(true)} > */}
-                    <Button style={{alignItems:'center', justifyContent:'space-between', flexDirection: 'row', height:40, borderRadius:40}} onPress={() => router.navigate({
+                <DefaultView style={{position:'absolute', bottom: bottom, left:'30%', right:'30%'}} >
+                    <Button 
+                        activeOpacity={0.8}
+                        style={[styles.boxWithShadow,{alignItems:'center', justifyContent:'center', flexDirection: 'row', height:40, borderRadius:40, backgroundColor:buttonColor}]} 
+                        onPress={() => router.navigate({
                         pathname:'/(app)/(modals)/[matchModal]',
                         params: { matchModal: false }
                         })} >
-                        <Text style={{ fontWeight:'600', textAlign:'center', flex:1, color:'white' }}>
+                        <Text style={{ fontWeight:'700', textAlign:'center', color:'white' }}>
                             New Match
                         </Text>
+                        <Animated.Text style={{marginLeft:6, transform:[{rotateZ:spin}]}}>
+                            ⚽
+                        </Animated.Text>
                     </Button>
                 </DefaultView>
             </View>
@@ -63,12 +113,22 @@ const matchesOrganized = () => {
                         setVisible={() => setShowNewMatchModal(false)}
                         allowDragDownToClose={true}
                     >
-                        <TopBarNavigator goBackAction={() => console.log('epa')} icon='check' activeColor   />
+                        <TopBarNavigator goBackAction={() => router.back()} icon='check' activeColor   />
                     </BottomModal>
                 </>
             }
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    boxWithShadow: {
+        shadowColor: '#000',
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,  
+        elevation: 5
+    }
+})
 
 export default matchesOrganized;
