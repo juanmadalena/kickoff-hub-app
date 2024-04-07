3be5cf4fabaf7d6b259e76739d83075f75d7f259
Juan Madalena <juanmadalena06@gmail.com>
2024-04-07 05:24:42 +0200

    calculating date offset

--- utils/calculateEndTime.ts
+++ utils/calculateEndTime.ts
@@ -2,20 +2,23 @@ export const calculateEndTime = (startDate: string, startTime: string, duration:
     try{
         const [startHour, startMinute] = startTime.split(':').map(Number);
         
+        const offset = new Date().getTimezoneOffset() * -1;
+
         // startDate + startTime
         let startDateFormatted = new Date(startDate);
         startDateFormatted.setHours(startHour);
-        startDateFormatted.setMinutes(startMinute);
-    
+        startDateFormatted.setMinutes(startMinute + offset);
+
         const [hours, minutes] = duration.split(':').map(Number);
-    
-        const endDate = new Date(startDateFormatted.getTime() + hours * 60 * 60 * 1000 + minutes * 60 * 1000);
-    
+
+        const endDate = new Date(startDateFormatted.getTime());
+        endDate.setHours(endDate.getHours() + hours);
+        endDate.setMinutes(endDate.getMinutes() + minutes);
+
         if(sumInMinutes) {
             const sumDate = new Date(endDate.getTime() + sumInMinutes * 60 * 1000);
             return sumDate;
         }
-    
         return endDate;
     }
     catch(e){
